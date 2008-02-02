c01047517acecfbc995216aa10da12d388b6cbf9
devin.papineau <devin.papineau@ca658394-2245-0410-bbc9-eb9d3805450a>
2008-02-02 03:18:55 +0000

    R_CreateWindow() now creates a window.

--- src/render/r_window.c
+++ src/render/r_window.c
@@ -1,5 +1,5 @@
 /******************************************************************************\
- Plutocracy - Copyright (C) 2008 - Michael Levin
+ Merchant Isles - Copyright (C) 2008 - Michael Levin, Devin Papineau
 
  This program is free software; you can redistribute it and/or modify it under
  the terms of the GNU General Public License as published by the Free Software
@@ -11,12 +11,47 @@
 \******************************************************************************/
 
 #include "r_common.h"
+#include "SDL.h"
+#include <stdlib.h>
 
 /******************************************************************************\
  Creates the client window. Returns TRUE on success.
 \******************************************************************************/
 int R_CreateWindow(void)
 {
+        SDL_Surface *screen;
+
         Debug("Hello World 2!");
-        return FALSE;
+
+        /* Just video for now. */
+        if(SDL_Init(SDL_INIT_VIDEO) < 0) {
+                return FALSE;
+        }
+
+        /* TODO: have configurable stuffs */
+
+        /* Set GL options: just something for now I guess
+         *  - 32bit colour
+         *  - 4-bit stencil
+         *  - double-buffered
+         *  - 16b depth
+         */
+        SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+        SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+        SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+        SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+        SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+        SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+
+        /* Open context. 800x600 for now. */
+        screen = SDL_SetVideoMode(800, 600, 0, SDL_OPENGL);
+        if(!screen) {
+                SDL_Quit();
+                return FALSE;
+        }
+
+        /* Make sure it's cleaned up. */
+        atexit(R_CloseWindow);
+
+        return TRUE;
 }
