5c1541c83a3d124faea86878eb9e91e86d131015
risujin <risujin@risujin.org>
2008-02-02 02:08:25 +0000

    LibSDL added to Autotools config

--- src/client.c
+++ src/client.c
@@ -1,12 +1,12 @@
 /******************************************************************************\
  Merchant Isles - Copyright (C) 2008 - Michael Levin
-  
- This program is free software; you can redistribute it and/or modify it under 
- the terms of the GNU General Public License as published by the Free Software 
+
+ This program is free software; you can redistribute it and/or modify it under
+ the terms of the GNU General Public License as published by the Free Software
  Foundation; either version 2, or (at your option) any later version.
 
- This program is distributed in the hope that it will be useful, but WITHOUT 
- ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 \******************************************************************************/
 
@@ -14,12 +14,14 @@
    also function as a server. */
 
 #include "common.h"
+#include "render/r_shared.h"
 
 /******************************************************************************\
  Start up the client program from here.
 \******************************************************************************/
 int main(int argc, char *argv[])
 {
+        R_CreateWindow();
         return 0;
 }
 
--- src/render/r_window.c
+++ src/render/r_window.c
@@ -1,14 +1,21 @@
 /******************************************************************************\
  Merchant Isles - Copyright (C) 2008 - Michael Levin
-  
- This program is free software; you can redistribute it and/or modify it under 
- the terms of the GNU General Public License as published by the Free Software 
+
+ This program is free software; you can redistribute it and/or modify it under
+ the terms of the GNU General Public License as published by the Free Software
  Foundation; either version 2, or (at your option) any later version.
 
- This program is distributed in the hope that it will be useful, but WITHOUT 
- ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 \******************************************************************************/
 
 #include "r_common.h"
 
+/******************************************************************************\
+ Creates the client window. Returns TRUE on success.
+\******************************************************************************/
+int R_CreateWindow(void)
+{
+        return FALSE;
+}
