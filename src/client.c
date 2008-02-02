8c324b166d81ab309c2295bdd17f15baf88d2a37
risujin <risujin@risujin.org>
2008-02-02 01:24:10 +0000

    First commit!

---
+++ src/client.c
@@ -0,0 +1,25 @@
+/******************************************************************************\
+ Merchant Isles - Copyright (C) 2008 - Michael Levin
+  
+ This program is free software; you can redistribute it and/or modify it under 
+ the terms of the GNU General Public License as published by the Free Software 
+ Foundation; either version 2, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT 
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+\******************************************************************************/
+
+/* This file forms the starting point for the client program. The client may
+   also function as a server. */
+
+#include "common.h"
+
+/******************************************************************************\
+ Start up the client program from here.
+\******************************************************************************/
+int main(int argc, char *argv[])
+{
+        return 0;
+}
+
